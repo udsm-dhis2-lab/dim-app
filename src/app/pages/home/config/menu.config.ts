ef0378fa69a6d5b792590d32de327ed4800afd93
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-08 15:48:24 +0300

    feat: 🎸 added menu config
    This commit will add menu config

---
+++ src/app/pages/home/config/menu.config.ts
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+import { Menu } from '../models/side-menu.model';
+/**
+ *
+ */
+export const SideMenuConfig: Array<Menu> = [
+    {
+        name: 'Create Integration',
+        route: 'create',
+    },
+    {
+        name: 'Configure Integration',
+        route: 'edit',
+    },
+    {
+        name: 'Integrations',
+        route: 'list',
+    },
+];
