74a71e68140504e0b701d8cf9a9e6adf67f0bde4
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-08 21:20:36 +0300

    feat: 🎸 added system integration state
    This commit will add system integration state

---
+++ src/app/pages/home/state/integration.state.ts
@@ -0,0 +1,51 @@
+/**
+ *
+ */
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
+import { SystemIntegration } from '../models/integration.model';
+import { HTTPErrorMessage } from 'src/app/shared/models/http-error.model';
+/**
+ *
+ */
+export interface SystemIntegrationState extends EntityState<SystemIntegration> {
+    selectedSystemIntegrationId: string | number | null;
+    loading: boolean;
+    loaded: boolean;
+    editing: boolean;
+    edited: boolean;
+    deleted: boolean;
+    created: boolean;
+    error: HTTPErrorMessage | null;
+    systemIntegration: SystemIntegration;
+}
+
+/**
+ *
+ */
+export const systemIntegrationAdapter: EntityAdapter<SystemIntegration> = createEntityAdapter<
+    SystemIntegration
+>();
+
+/**
+ *
+ */
+export const defaultSystemIntegration: SystemIntegrationState = {
+    ids: [],
+    entities: {},
+    selectedSystemIntegrationId: null,
+    loaded: false,
+    loading: false,
+    editing: false,
+    edited: false,
+    deleted: false,
+    created: false,
+    error: null,
+    systemIntegration: null,
+};
+
+/**
+ *
+ */
+export const initialReportState = systemIntegrationAdapter.getInitialState(
+    defaultSystemIntegration
+);
