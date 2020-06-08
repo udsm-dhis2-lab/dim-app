d41b72bbd0a5d74ca93986f9dcb66961b2a33ac1
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-08 09:24:30 +0300

    fix: 🐛 exported navigation header in shared module
    This commit will export navigation header component in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,14 @@
  */
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
 /**
  *
  */
 @NgModule({
+  declarations: [NavigationMenuComponent],
   imports: [CommonModule],
-  exports: [],
-  declarations: [],
+  exports: [NavigationMenuComponent],
 })
 /**
  *
