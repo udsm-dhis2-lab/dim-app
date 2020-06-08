d7204f2882bba0a58726851508c4c6292b6d2c5a
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-08 10:41:37 +0300

    feat: 🎸 added shared menu configuration
    This commit will add shared menu configuration

---
+++ src/app/shared/config/menu.config.ts
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+import { NavigationMenu } from '../models/menu.model';
+
+/**
+ *
+ */
+export const NavigationMenuConfig: Array<NavigationMenu> = [
+    {
+        name: 'Integration Management',
+    },
+    {
+        name: 'Batch Management',
+    },
+    {
+        name: 'Job Management',
+    },
+    {
+        name: 'Report Management',
+    },
+    {
+        name: 'Authentication Management',
+    }
+];
