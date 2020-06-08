b81c592be0e56b2cea9c0600d9272fbdc106756d
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-08 09:33:57 +0300

    feat: 🎸 added integration management component
    This commit will add integration management component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { pages } from './pages';
+import { IntegrationManagementComponent } from './components/integration-management/integration-management.component';
 
 @NgModule({
-  declarations: [...pages],
-  imports: [CommonModule, SharedModule, HomeRoutingModule]
+  declarations: [IntegrationManagementComponent],
+  imports: [CommonModule, SharedModule, HomeRoutingModule],
 })
 export class HomeModule {}
