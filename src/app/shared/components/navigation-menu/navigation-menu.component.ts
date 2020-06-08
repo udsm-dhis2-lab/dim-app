cc348f225af4c3b984beb17d4f6d633f48578d30
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-08 10:49:42 +0300

    fix: 🐛 updated navigation menu
    This commit will update navigation menu

--- src/app/shared/components/navigation-menu/navigation-menu.component.ts
+++ src/app/shared/components/navigation-menu/navigation-menu.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ *
+ */
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+/**
+ *
+ */
+import { NavigationMenu } from '../../models/menu.model';
+import { NavigationMenuConfig } from '../../config/menu.config';
 
 @Component({
   selector: 'app-navigation-menu',
   templateUrl: './navigation-menu.component.html',
-  styleUrls: ['./navigation-menu.component.css']
+  styleUrls: ['./navigation-menu.component.css'],
 })
-export class NavigationMenuComponent implements OnInit {
+export class NavigationMenuComponent implements OnInit, OnDestroy {
+  /**
+   *
+   */
+  navigationMenu: Array<NavigationMenu> = NavigationMenuConfig;
 
-  constructor() { }
+  /**
+   *
+   */
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  /**
+   *
+   */
+  ngOnInit(): void {}
 
+  /**
+   *
+   */
+  ngOnDestroy(): void {}
+
+  /**
+   *
+   */
+  onOpenMenu() {}
 }
