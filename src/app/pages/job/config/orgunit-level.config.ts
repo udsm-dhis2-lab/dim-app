ace1dab8d0113b5034c1cd5d4eb5a75d11a49d86
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-09 17:25:02 +0300

    feat: 🎸 added organisation unit config
    This commit will add organisation unit config

---
+++ src/app/pages/job/config/orgunit-level.config.ts
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+import { OrgUnitLevel } from '../models/orgunit-level.model';
+/**
+ *
+ */
+export const OrgUnitLevelConfig: Array<OrgUnitLevel> = [
+    {
+        name: 'Level 1',
+        value: 1,
+    },
+    {
+        name: 'Level 2',
+        value: 2,
+    },
+    {
+        name: 'Level 3',
+        value: 3,
+    },
+    {
+        name: 'Level 4',
+        value: 4,
+    },
+];
