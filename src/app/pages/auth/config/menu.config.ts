2ff3b067654530a9e0d96711750f5be0c4fa36bb
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-09 19:47:16 +0300

    feat: 🎸 added auth configuration
    This commit will add auth configuration

---
+++ src/app/pages/auth/config/menu.config.ts
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+import { Menu } from 'src/app/shared/models/side-menu.model';
+/**
+ *
+ */
+export const AuthSideMenuConfig: Array<Menu> = [
+    {
+        name: 'Create Auth',
+        route: 'create',
+    },
+];
