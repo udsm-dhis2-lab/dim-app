8f260840045b046c585f0a8c47de83b1e4f6f253
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-09 12:21:12 +0300

    feat: 🎸 added job forms
    This commit will add jobs creation form

--- src/app/pages/job/job.module.ts
+++ src/app/pages/job/job.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { JobRoutingModule } from './job-routing.module';
 import { JobComponent } from './components/job/job.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CreateJobComponent } from './components/form/create-job/create-job.component';
+import { EditJobComponent } from './components/form/edit-job/edit-job.component';
 
 
 @NgModule({
-  declarations: [JobComponent],
+  declarations: [JobComponent, CreateJobComponent, EditJobComponent],
   imports: [
     CommonModule,
     JobRoutingModule,
