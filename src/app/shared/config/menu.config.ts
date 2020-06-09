7131f6db55b43efae37fcf5693aa3bfcbbfad042
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-09 18:44:45 +0300

    fix: 🐛 updated app configuration
    This commit will add updated app config

--- src/app/shared/config/menu.config.ts
+++ src/app/shared/config/menu.config.ts
@@ -8,6 +8,10 @@ import { NavigationMenu } from '../models/menu.model';
  */
 export const NavigationMenuConfig: Array<NavigationMenu> = [
     {
+        name: 'System Management',
+        route: 'system'
+    },
+    {
         name: 'Job Management',
         route: 'job'
     },
