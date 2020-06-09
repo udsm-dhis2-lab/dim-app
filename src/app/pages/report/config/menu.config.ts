48c131b8066a9c8630cd5c082a02cafd7180f1cd
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-10 00:39:31 +0300

    feat: 🎸 added support for report config
    This commit will add report config

---
+++ src/app/pages/report/config/menu.config.ts
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+import { Menu } from 'src/app/shared/models/side-menu.model';
+/**
+ *
+ */
+export const ReportSideMenuConfig: Array<Menu> = [
+    {
+        name: 'Generate Report',
+        route: 'generate',
+    },
+];
