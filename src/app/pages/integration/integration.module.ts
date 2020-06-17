482d0fd6e8b24a677fe4b313b3bc66c3a5148331
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-17 19:58:19 +0300

    feat: 🎸 added integration module
    This commit will add integration module

---
+++ src/app/pages/integration/integration.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IntegrationRoutingModule } from './integration-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IntegrationRoutingModule
+  ]
+})
+export class IntegrationModule { }
