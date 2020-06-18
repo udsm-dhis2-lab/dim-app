3f26048406991e44438c2f164436e34c6c6499d7
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-18 10:31:04 +0300

    feat: 🎸 added support for batch service menu config
    This commit will add support for batch service menu config

---
+++ src/app/pages/batch/config/service.config.ts
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+import { MenuInfo } from '../../../shared/models/service.model';
+/**
+ *
+ */
+export const BatchMenu: Array<MenuInfo> = [
+    {
+        title: 'Configure Batch',
+        description:
+            ' Create, modify, view and delete Batch. Batch is the core part to facilitate integration process between source and recepient system',
+        routeCreate: 'create',
+        routeList: 'list',
+    },
+];
