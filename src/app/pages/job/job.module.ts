8c79a7c649da7bc9f57a5d44c9e2054be1f8f6ac
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-18 09:45:34 +0300

    fix: 🐛 updated job routes in job routing module
    This commit will update routes in the job routing module

--- src/app/pages/job/job.module.ts
+++ src/app/pages/job/job.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { JobRoutingModule } from './job-routing.module';
-import { JobComponent } from './components/job/job.component';
+import { JobComponent } from './containers/job/job.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CreateJobComponent } from './components/form/create-job/create-job.component';
 import { EditJobComponent } from './components/form/edit-job/edit-job.component';
 import { DataElementSelectionComponent } from './components/selection/data-element-selection/data-element-selection.component';
 import { IndicatorSelectionComponent } from './components/selection/indicator-selection/indicator-selection.component';
 import { ProgramIndicatorSelectionComponent } from './components/selection/program-indicator-selection/program-indicator-selection.component';
+import { ServiceMenuComponent } from './components/service-menu/service-menu.component';
+import { JobListComponent } from './components/job-list/job-list.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { ProgramIndicatorSelectionComponent } from './components/selection/progr
     DataElementSelectionComponent,
     IndicatorSelectionComponent,
     ProgramIndicatorSelectionComponent,
+    ServiceMenuComponent,
+    JobListComponent,
   ],
   imports: [CommonModule, JobRoutingModule, SharedModule],
 })
