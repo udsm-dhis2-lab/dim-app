52cf85726ba81ef87e53a08f48c90920e502c9de
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-18 09:43:54 +0300

    feat: 🎸 added support for job service configuration
    This commit will add support for job service configuration

---
+++ src/app/pages/job/config/service.config.ts
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+import { MenuInfo } from '../../../shared/models/service.model';
+/**
+ *
+ */
+export const JobMenu: Array<MenuInfo> = [
+    {
+        title: 'Configure Jobs',
+        description:
+            ' Create, modify, view and delete Jobs. Job is the core part to facilitate integration process between source and recepient systems',
+        routeCreate: 'create',
+        routeList: 'list',
+    },
+];
