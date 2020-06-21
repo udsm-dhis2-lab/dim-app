1f3eb268ab5083b27c9f430bf614578b15b90394
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-21 19:25:13 +0300

    feat: 🎸 added support for organisation unit levels
    This commit will add support for organisation unit levels

---
+++ src/app/pages/job/config/orgunit-level.config.ts
@@ -0,0 +1,22 @@
+export const OrganisationUnitLevels = [
+    {
+        name: 'Level 1',
+        level: '1',
+    },
+    {
+        name: 'Level 2',
+        level: '2',
+    },
+    {
+        name: 'Level 3',
+        level: '3',
+    },
+    {
+        name: 'Level 4',
+        level: '4',
+    },
+    {
+        name: 'Level 5',
+        level: '5',
+    },
+];
