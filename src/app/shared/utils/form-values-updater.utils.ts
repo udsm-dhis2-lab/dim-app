80fe99c3d8977ddc7ab84b87688c0806be5bf192
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-24 07:17:16 +0300

    fix: 🐛 updated form values updater utility
    This commit will update form values updater utility

--- src/app/shared/utils/form-values-updater.utils.ts
+++ src/app/shared/utils/form-values-updater.utils.ts
@@ -11,5 +11,5 @@ export function onUpdateFormProps(
     formValues: any,
     formUpdates: DataEntryField
 ) {
-    return formUpdates ? _.merge(_.clone(formValues), formUpdates) : formUpdates;
+    return formUpdates ? { ..._.clone(formValues), ...formUpdates } : formUpdates;
 }
