d53f2b5d2d5dfb5f39d2b6d67d90bb426d8c498a
Walter V. Faustine <waltervfaustine@gmail.com>
2020-06-24 13:51:04 +0300

    feat: 🎸 added auth service config
    This commit will add support for auth service config

---
+++ src/app/pages/auth/config/service.config.ts
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+import { MenuInfo } from '../../../shared/models/service.model';
+/**
+ *
+ */
+export const AuthMenu: Array<MenuInfo> = [
+    {
+        title: 'Configure Auth',
+        description:
+            ' Create, modify, view and delete Auth Configuration. Auth configuration helps in linkage between two systems for the purpose of data exchange',
+        routeCreate: 'create',
+        routeList: 'list',
+    },
+];
