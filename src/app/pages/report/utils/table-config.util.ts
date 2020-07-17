1ed9c313b87ebfb2866d81d75e29ecda1722f2e5
Walter V. Faustine <waltervfaustine@gmail.com>
2020-07-17 19:36:43 +0300

    feat: 🎸 added support for table utils configurations
    This commit will add support for table utils configurations

---
+++ src/app/pages/report/utils/table-config.util.ts
@@ -0,0 +1,14 @@
+export const ReportTableCulumns: Array<string> = [
+    'sourceSystemName',
+    'sourceSystemId',
+    'sourceExchangePeriod',
+    'sourceExchangeDate',
+    'sourceValue',
+    'sourceDataElementName',
+    'sourceDataElementId',
+    'sourceOrgUnitId',
+    'destinationSystemName',
+    'destinationSystemId',
+    'destinationDataSetName',
+    'destinationDataSetId'
+];
